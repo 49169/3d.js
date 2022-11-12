1f145573c0599f42b1ad2660635aaf08d47a9995
Kevin Li <cyberpatriot49169@gmail.com>
2022-11-12 17:47:47 -0500

    Added Cube

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
- import './styles/main.css'
- import * as THREE from 'three'
+import * as THREE from '/node_modules/three/build/three.module.js';
 
- const scene = new THREE.Scene();
- const camera = new THREE.PerspectiveCamera();
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1,1,1);
+var  material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var  cube = new THREE.Mesh(geometry,material);
+
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+}
+animate();
